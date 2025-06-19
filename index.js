94b4eba3e69d361212ffb5cbe0a2a85c22231469
John Caudill <jcaudill86@gmail.com>
2025-06-19 15:35:08 -0400

    Thursday Push

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-//grab menu class and give it an open or closed option
+//intention of below code: if menu is hidden on button click,
+//change style to visible on click.
+//else if visible on click, make hidden.
+
+var button = document.querySelector('button.hamburger-btn');
+var buttonElement = document.getElementById('button');
+
+
+button.onclick = function() {
+
+if(button.style.visibility = 'hidden'){
+    button.setAttribute('aria-expanded', 'true')
+    button.style.visibility = 'visible'
+   } else{ button.style.visibility = 'hidden'
+
+   }
+
+   console.log("click");
+};
+
+
+
+
+
+/*
+Nothing below here has worked thus far
+
+var hamburgerBtn = document.querySelector('button.hambuger-btn')
+
+hamburgerBtn.onclick() 
+    hamburgerBtn.classList.toggle('active');
+    mainMenu.classList.toggle('active');
+    ariaExpanded.classList.toggle('true');
+
+
+
+
 
 const hamburgerBtn = document.querySelector('button.hamburger-btn');
 const hamburgerMenu = document.querySelector('ul.hamburger-menu')
 const mainMenu = document.querySelector('nav.menu')
+const ariaExpanded = document.querySelector('button.aria-expanded')
 
 hamburgerBtn.addEventListener('click', () =>{
     hamburgerBtn.classList.toggle('active');
     mainMenu.classList.toggle('active');
+    ariaExpanded.classList.toggle('true');
 });
 
 
